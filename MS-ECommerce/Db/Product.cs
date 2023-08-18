ea0b2ced2f760a54b8ea472c3d6e4e257b57d890
silcwb <silvana.siqueira@gmail.com>
2023-08-18 01:19:01 -0300

    class updated

--- MS-ECommerce/Db/Product.cs
+++ MS-ECommerce/Db/Product.cs
@@ -4,8 +4,11 @@ namespace MS_ECommerce.Db
 	public class Product
 	{
 		public int Id { get; set; }
+
 		public required string Name { get; set; }
+
 		public decimal Price { get; set; }
+
 		public int Inventory { get; set; }
 	}
 }
