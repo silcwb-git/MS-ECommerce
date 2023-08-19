3f8d9ffcb6f1fde713cab0a676dbbffc11dd2ea2
silcwb <silvana.siqueira@gmail.com>
2023-08-19 19:48:06 -0300

    created profiles layer

---
+++ ECommerce.Api.Orders/Profiles/OrderItemsProfile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ECommerce.Api.Orders.Profiles
+{
+    public class OrderItemsProfile : AutoMapper.Profile
+    {
+        public OrderItemsProfile()
+        {
+            CreateMap<Db.OrderItem, Models.OrderItem>();
+        }
+    }
+}
+
---
+++ ECommerce.Api.Orders/Profiles/OrdersProfile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ECommerce.Api.Orders.Profiles
+{
+    public class OrdersProfile : AutoMapper.Profile
+    {
+        public OrdersProfile()
+        {
+            CreateMap<Db.Order, Models.Order>();
+        }
+    }
+}
+
